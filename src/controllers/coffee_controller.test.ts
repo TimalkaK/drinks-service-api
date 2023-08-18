fd2ee61b8a435c229071bf6e7313aef83f9d895a
TimalkaK <timalkamk@gmail.com>
2023-08-18 17:10:27 +0100

    feat:Implemented tests for different coffee and no query params

--- src/controllers/coffee_controller.test.ts
+++ src/controllers/coffee_controller.test.ts
@@ -4,12 +4,36 @@ import { app } from '../app';
 
 // As a user, when I do a GET request to the /coffee endpoint, I will see the JSON response which includes two properties: the drinkType set to “Coffee” and name set to the provided name
 test('GET /coffee should return correct object', async () => {
-const res = await request(app)
-.get('/coffee')
-.query({ coffeeName: 'Latte' });
-expect(res.statusCode).toEqual(200);
-expect(res.body).toEqual({
-drinkType: 'Coffee',
-name: 'Latte',
-});
-});
+  const res = await request(app)
+  .get('/coffee')
+  .query({ coffeeName: 'Latte' });
+  expect(res.statusCode).toEqual(200);
+  expect(res.body).toEqual({
+  drinkType: 'Coffee',
+  name: 'Latte',
+  });
+  });
+
+  test('GET /coffee should return a different coffee name, Flat White', async () => {
+    const res = await request(app)
+    .get('/coffee')
+    .query({ coffeeName: 'Flat White' });
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({
+    drinkType: 'Coffee',
+    name: 'Flat White',
+    });
+    });
+
+    test('GET /coffee should return correct object without specified coffee name parameter', async () => {
+      const res = await request(app)
+      .get('/coffee')
+      expect(res.statusCode).toEqual(200);
+      expect(res.body).toEqual({
+      drinkType: 'Coffee',
+      name: 'Latte',
+      });
+      });
+
+
+
